b8c16faf655cdb122f04787df1ffe83947c8f13d
OpenActiveSA <104970031+OpenActiveSA@users.noreply.github.com>
2025-10-19 09:10:29 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+
+function Home() {
+  return <div className="page">Open Active Home</div>
+}
+
+function Login() {
+  return <div className="page">Open Login</div>
+}
+
+function DemoClub() {
+  return <div className="page">Demo Club</div>
+}
+
+function DemoClubLogin() {
+  return <div className="page">Demo Club Login</div>
+}
 
 function App() {
   return (
-    <div className="app">
-      <div className="container">
-        <h1 className="title">Open Active</h1>
-        <p className="subtitle">Tennis Booking System</p>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/club/demo" element={<DemoClub />} />
+        <Route path="/club/demo/login" element={<DemoClubLogin />} />
+      </Routes>
+    </Router>
   )
 }
 
